9eb7c57f0b574e12259e9c6e4c0b124c29504470
Victor Ezekiel <43010620+ibkvictor@users.noreply.github.com>
2020-07-15 16:09:24 +0100

    Create EnergySupplierData.js
    This contains the energy supplier data used by https://www.greenthelight.co/

---
+++ EnergySupplierData.js
@@ -0,0 +1,183 @@
+export function fetcher(){
+    return({
+   user:[
+      {
+         "Country":"Ireland",
+         "Clean Energy Rank":1,
+         "Company name":"Community Power Electricity",
+         "Verdict on Clean Energy Approach":"The Company purchases hydro and wind  renewable energy generators across Ireland and sells them.",
+         "Link":"https://communitypower.ie/",
+         "image_link":""
+      },
+      {
+         "Country":"Ireland",
+         "Clean Energy Rank":2,
+         "Company name":"Energia Ireland",
+         "Verdict on Clean Energy Approach":"This Company is 3rd biggest supplier and promotes energy waste reduction systems.",
+         "Link":"https://www.energia.ie/home",
+         "image_link":""
+      },
+      {
+         "Country":"Ireland",
+         "Clean Energy Rank":3,
+         "Company name":"Pinergy",
+         "Verdict on Clean Energy Approach":"The Company 100% green electricity and free low-consumption LED bulbs.",
+         "Link":"https://pinergy.ie/",
+         "image_link":""
+      },
+      {
+         "Country":"Ireland",
+         "Clean Energy Rank":4,
+         "Company name":"Electric Ireland",
+         "Verdict on Clean Energy Approach":"51.3% of dependance of Gas and 37.7% is the ratio of their Renewable Energy.",
+         "Link":"https://www.electricireland.ie/",
+         "image_link":""
+      },
+      {
+         "Country":"Ireland",
+         "Clean Energy Rank":5,
+         "Company name":"Be Energy",
+         "Verdict on Clean Energy Approach":"23.9% is renewable sourced energy and 17.9% of Coal based energy.",
+         "Link":"https://beenergy.ie/",
+         "image_link":""
+      },
+      {
+         "Country":"Japan",
+         "Clean Energy Rank":1,
+         "Company name":"Green People’s Power",
+         "Verdict on Clean Energy Approach":"Aims to supply  bimoassrenewable energy based on 100% citizen participation.",
+         "Link":"https://www.greenpeople.co.jp/",
+         "image_link":""
+      },
+      {
+         "Country":"Japan",
+         "Clean Energy Rank":2,
+         "Company name":"Shizen Denki by SoftBank",
+         "Verdict on Clean Energy Approach":"The Company does not charge a basic fee and 80% ratio of renewable energy (2019)",
+         "Link":"https://www.softbank.jp/energy/special/shizen-denki/",
+         "image_link":""
+      },
+      {
+         "Country":"Japan",
+         "Clean Energy Rank":3,
+         "Company name":"Greena Denki :",
+         "Verdict on Clean Energy Approach":"Claims to be the first Japanese company that supplies 100% renewable energy.",
+         "Link":"https://ne-greena.jp/",
+         "image_link":""
+      },
+      {
+         "Country":"Japan",
+         "Clean Energy Rank":4,
+         "Company name":"Kansai Electric Power Company",
+         "Verdict on Clean Energy Approach":" 2nd Largest supplier in Japan their renewable energy share doesn’t exceed 4% (2019)",
+         "Link":"https://www.kepco.co.jp/",
+         "image_link":""
+      },
+      {
+         "Country":"Japan",
+         "Clean Energy Rank":5,
+         "Company name":"Tokyo Electric Power Company",
+         "Verdict on Clean Energy Approach":"The largest supplier in Japan but has only 58 units of renewable sources.",
+         "Link":"https://www.tepco.co.jp/",
+         "image_link":""
+      },
+      {
+         "Country":"USA",
+         "Clean Energy Rank":1,
+         "Company name":"Commonwealth Fusion",
+         "Verdict on Clean Energy Approach":"CFS has assembled a world-class team working to design and build fusion.",
+         "Link":" https://cfs.energy/",
+         "image_link":""
+      },
+      {
+         "Country":"USA",
+         "Clean Energy Rank":2,
+         "Company name":"Atlas Energy Systems",
+         "Verdict on Clean Energy Approach":"Developing solid-state energy converters for waste heat power production and concentrated solar.",
+         "Link":"https://atlasenergysys.com/",
+         "image_link":""
+      },
+      {
+         "Country":"USA",
+         "Clean Energy Rank":3,
+         "Company name":" GE Power",
+         "Verdict on Clean Energy Approach":"Uses both renewable resources such as water, wind, solar and biogas; alongside coal, oil, natural gas.",
+         "Link":"https://www.ge.com/power",
+         "image_link":""
+      },
+      {
+         "Country":"USA",
+         "Clean Energy Rank":4,
+         "Company name":"CPL Retail Energy",
+         "Verdict on Clean Energy Approach":"Does not have a 100% green energy plan, but has fixed and variable plans for 100% natural gas.",
+         "Link":"https://www.cplretailenergy.com/",
+         "image_link":""
+      },
+      {
+         "Country":"USA",
+         "Clean Energy Rank":5,
+         "Company name":"Infinite Energy",
+         "Verdict on Clean Energy Approach":"Offers energy managment and supply but with limited green solutions.",
+         "Link":"https://www.infiniteenergy.com/",
+         "image_link":""
+      },
+      {
+         "Country":"South Africa",
+         "Clean Energy Rank":1,
+         "Company name":"Eskom",
+         "Verdict on Clean Energy Approach":"Largest supplier in SA but launched US$11 Billion Green-Energy Initiative.",
+         "Link":"http://www.eskom.co.za/",
+         "image_link":""
+      },
+      {
+         "Country":"South Africa",
+         "Clean Energy Rank":2,
+         "Company name":"AGE Technologies",
+         "Verdict on Clean Energy Approach":"Leading Electrical Engineering company with a number of solutions for our clients' green energy needs.",
+         "Link":"https://www.age.co.za",
+         "image_link":""
+      },
+      {
+         "Country":"South Africa",
+         "Clean Energy Rank":3,
+         "Company name":"Sasol",
+         "Verdict on Clean Energy Approach":"Chemicals and energy company tlaunching a program to procure some 600 MW of renewable power.",
+         "Link":"https://www.sasol.com/",
+         "image_link":""
+      },
+      {
+         "Country":"South Africa",
+         "Clean Energy Rank":4,
+         "Company name":"Exxaro",
+         "Verdict on Clean Energy Approach":"Provides Coal based energy but hopes dropping 22% of coal ressources over time.",
+         "Link":"https://www.exxaro.com/",
+         "image_link":""
+      },
+      {
+         "Country":"South Africa",
+         "Clean Energy Rank":5,
+         "Company name":"AngloGold Ashanti",
+         "Verdict on Clean Energy Approach":"Ressources based on mining based energy coming with high costs and constraints.",
+         "Link":"https://www.anglogoldashantisouthafrica.co.za/",
+         "image_link":""
+      },
+      {
+         "Country":"UAE",
+         "Clean Energy Rank":1,
+         "Company name":"Masdar: Shams 1",
+         "Verdict on Clean Energy Approach":"Targets  7 percent power-generation capacity via renewable energy by depicting heat instead of solar energy.",
+         "Link":"https://masdar.ae/en/masdar-clean-energy/projects/shams-1",
+         "image_link":""
+      },
+      {
+         "Country":"UAE",
+         "Clean Energy Rank":2,
+         "Company name":"Noor",
+         "Verdict on Clean Energy Approach":"Aimes to increase the existing solar capacity from 110 MW to 1,287 MW and grow solar capacity of the UAE.",
+         "Link":"http://www.ewec.ae/en/noor-abu-dhabi.html",
+         "image_link":""
+      }
+   ]
+
+    });
+}
